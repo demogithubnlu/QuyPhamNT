0aad9e3d06296a034c185a907f010caea362557d
quyphamk14nlu <nguyennhatminh96itnl@gmail.com>
2018-07-13 15:18:28 +0700

    commit nguyen nhat minh

--- PhamHuuQuy/src/QuyPhamNLU.java
+++ PhamHuuQuy/src/QuyPhamNLU.java
@@ -3,7 +3,8 @@ public class QuyPhamNLU {
 
 	public static void main(String[] args) {
 		System.out.println("Hello pham huu quy");
-
+		//
+		System.out.println("how are you doing today?");
 	}
 
 }

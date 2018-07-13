f9399aa0d26e1b5000ccf66ce394c9d8cda10a45
demogithubnlu <14130101@st.hcmuaf.edu.vn>
2018-07-13 15:15:01 +0700

    commit foork

--- PhamHuuQuy/src/QuyPhamNLU.java
+++ PhamHuuQuy/src/QuyPhamNLU.java
@@ -2,7 +2,10 @@
 public class QuyPhamNLU {
 
 	public static void main(String[] args) {
+		
+		//print how are you doing today?
 		System.out.println("Hello pham huu quy");
+		System.out.println("===========");
 
 	}
 
